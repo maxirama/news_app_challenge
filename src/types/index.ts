6de442d21e525c2ac8b0d05ced8e85478138e338
maxirama <maximilianogastonama@gmail.com>
2024-09-16 01:05:51 -0300

    feat: agregado de interfaces. progresos en creación de estado global.

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export interface IArticle {
+  source: {
+    id: string | null;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+}
+
+export interface INewsApiResponse {
+  status: string;
+  totalResults: number;
+  articles: IArticle[];
+}
